7d749939640f35c30752a221ba8b123ce87328ae
idanhersh <107764469+idanhersh@users.noreply.github.com>
2023-06-18 20:43:47 +0300

    final.py

--- final.py
+++ final.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 #Import dataset
